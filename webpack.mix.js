b2b96b551b2e4142f1f60f7afe42f6165094e680
wanglelecc <wanglelecc@gmail.com>
2018-04-01 23:28:12 +0800

    添加首页

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,28 @@ const { mix } = require('laravel-mix');
  |
  */
 // mix.setPublicPath('public');
-mix.setPublicPath('dist');
-mix.js('src/js/app.js', 'dist/js').sass('src/sass/app.scss', 'dist/css');
+
+mix.setPublicPath('public').options({
+    fileLoaderDirs: {
+        images: 'images',
+        fonts: 'fonts'
+    }
+});
+
+mix.js('src/js/app.js', 'js')
+    .sass('src/sass/app.scss', 'css')
+    .copyDirectory('src/images', 'public/images')
+;
+
+mix.browserSync({
+    port: 5000,
+    proxy: 'assets.56br.cc', // 这里修改成当前项目域名
+    // 这里配置监控目录，只要符合规则的文件被修改，会立即发布资源并刷新页面（只有在 npm run watch 模式下哦）
+    files: [
+        'public/**/*.html',
+		'src/js/**/*.js',
+		'src/sass/**/*.sass'
+    ]
+});
+
+mix.disableNotifications();
